108fc00e3fe82017b0a5467ab0f51528da98db1c
Justin Galicic <jgalicic@gmail.com>
2019-03-01 10:27:11 -0800

    added instructions

--- lovebank.py
+++ lovebank.py
@@ -2,5 +2,5 @@ love_bank = ['SWEETIE', 'DOVE', 'POPPET', 'LOVEY', 'PUMPKIN', 'SUGAR', 'BABY', '
              'MUFFIN', 'BOO', 'ANGEL', 'SUNSHINE', 'DOLL', 'HUBBY', 'WIFEY', 'CUTIE',
              'DARLING', 'GORGEOUS', 'KITTEN', 'HOTTIE', 'LOVEBIRD', 'DEAR', 'PRETTY', 'SEXY',
              'MAMA', 'PAPA', 'SUNFLOWER', 'TWINKLES', 'AMIGO', 'BEAU', 'CHIPMUNK', 'DADDY',
-             'DUMPLING', 'FOXY', 'HERCULES', 'HOTSHOT', 'MEATBALL', 'PAPA', 'POOKIE', 'SHMOOPIE',
+             'DUMPLING', 'FOXY', 'HERCULES', 'HOTSHOT', 'MEATBALL', 'POOKIE', 'SHMOOPIE',
              'SNOOKUMS', 'TIGER', 'WOOKIE', ]
